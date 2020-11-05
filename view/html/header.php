9b20f4f0e2f77017160934d6e34adc325a5ae952
donnettew <william.donnette@etu.umontpellier.fr>
2020-11-05 12:06:54 +0100

    CSS HEADER

--- view/html/header.php
+++ view/html/header.php
@@ -1,13 +1,20 @@
 <header>
-    <a href="index.php" id="accueil">
+    <a href="index.php" id="accueil" class="col-3">
         <p id="logo">
             NOM DU PROJET
         </p>
     </a>
-    <input type="text" id="recherche" name="recherche">
-    <nav id="navigation">
-        <div id="panier">
-
+    <input type="text" id="recherche" name="recherche" class="col-5" placeholder="Rechercher">
+    <nav id="navigation" class="col-4">
+        <div id="panier" class="item box">
+            <img src="view/img/panier.png" alt="panier">
+        </div>
+        <div id="profil" class="item box">
+            <img src="view/img/profil.png" alt="profil">
+            <div id="nav_profil">
+                <p>Profil</p>
+                <p>Commandes</p>
+            </div>
         </div>
     </nav>
 </header>
