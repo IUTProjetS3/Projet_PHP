b85ec0712c36b20fb667266ece04d61212d1e967
donnettew <william.donnette@etu.umontpellier.fr>
2020-11-05 11:11:51 +0100

    index

--- index.php
+++ index.php
@@ -2,3 +2,23 @@
 $DS = DIRECTORY_SEPARATOR;
 require_once __DIR__ . $DS . 'lib' . $DS . 'File.php';
 ?>
+
+
+<!---HTML--->
+<?php
+$CSS = [
+    'view/css/header.css',
+    'view/css/style.css',
+];
+
+$JS = [
+
+];
+
+$LIBRAIRIES = [
+
+];
+require 'view/html/html_start.php';
+require 'view/html/header.php';
+require 'view/html/html_end.php';
+?>
