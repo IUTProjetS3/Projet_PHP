94ccaef2e0db904cd8a119338b5e3bfeecf1cc50
Adrien Segura <adrien.segura@etu.umontpellier.fr>
2020-11-05 14:03:02 +0100

    Mail

--- controller/ControllerUtilisateur.php
+++ controller/ControllerUtilisateur.php
@@ -127,7 +127,7 @@
 		     'Reply-To: webmaster@example.com' . "\r\n" .
 		     'X-Mailer: PHP/' . phpversion();
 
-		     $success = mail($to, $subject, $message, $headers, "noreply-libraire@gmail.com");
+		     $success = mail($to, $subject, $message, $headers);
 
 		     if (!$success) {
 				    $errorMessage = error_get_last()['message'];
