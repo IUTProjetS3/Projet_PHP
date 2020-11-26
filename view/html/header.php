5ad6f74b3781349ac1caa74337a39c968bb09c77
donnettew <william.donnette@etu.umontpellier.fr>
2020-11-26 11:01:44 +0100

    deconnexion

--- view/html/header.php
+++ view/html/header.php
@@ -14,11 +14,13 @@
             <div id="nav_profil">
                 <?php if(isset($_SESSION['projet_user_connected'])):?>
                     <a href="index.php?controller=utilisateur&action=profil&id=<?=$_SESSION['projet_user_connected']->getAttr("idUtilisateur")?>">Profil</a>
-                    <a href="index.php?action=deconnexion&controller=utilisateur">Déconnexion</a>
                 <?php else : ?>
                     <a href="index.php?controller=utilisateur&action=connexion">Connexion</a>
                 <?php endif; ?>
                 <p>Commandes</p>
+                <?php if(isset($_SESSION['projet_user_connected'])):?>
+                    <a href="index.php?action=deconnexion&controller=utilisateur">Déconnexion</a>
+                <?php endif; ?>
             </div>
         </div>
     </nav>
